b14715646bb627cdbf8515520ee13ea0808a0fe1
Boris Sargsyan <108880847+lokodayte@users.noreply.github.com>
2025-11-10 16:17:56 +0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Initialize Firebase for GitHub Pages (compat version)
+const firebaseConfig = {
+  apiKey: "AIzaSyA09Fms6TMFxO2NS69EP_baPltcQCieefU",
+  authDomain: "bolik-4c5b7.firebaseapp.com",
+  projectId: "bolik-4c5b7",
+  storageBucket: "bolik-4c5b7.appspot.com",  // <- notice it's appspot.com not firebasestorage.app
+  messagingSenderId: "893615231073",
+  appId: "1:893615231073:web:b2f6a514a417325b211a20",
+  measurementId: "G-N4CEGDDVS6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
