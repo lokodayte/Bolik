94b1300f6117d2325310d1fbbf15ca3747be7642
Boris Sargsyan <108880847+lokodayte@users.noreply.github.com>
2025-11-20 00:50:11 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA09Fms6TMFxO2NS69EP_baPltcQCieefU",
   authDomain: "bolik-4c5b7.firebaseapp.com",
   projectId: "bolik-4c5b7",
-  storageBucket: "bolik-4c5b7.appspot.com",  // <- notice it's appspot.com not firebasestorage.app
+  storageBucket: "bolik-4c5b7.appspot.com",
   messagingSenderId: "893615231073",
   appId: "1:893615231073:web:b2f6a514a417325b211a20",
   measurementId: "G-N4CEGDDVS6"
